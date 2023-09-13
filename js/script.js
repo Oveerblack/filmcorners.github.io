fae32c8ec1c452e8fa00612e179b0bfc9106b04c
Oveerblack <136482712+Oveerblack@users.noreply.github.com>
2023-09-13 18:49:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let movies = [
+
+    {
+        name:'Sihirli Annem',
+        des:'Başrollerini İnci Türkay ve Şahap Sayılgan ın paylaştığı dizi, Betüş isimli bir perinin, eşi vefat eden iki çocuk babası Sadık ile evlenmesi sonucu başlarından geçen macera dolu hayatlarını konu alır.',
+        image:'images/sihirliannem.png'
+    },
+    {
+        name:'Bizim Aile',
+        des:'Melek Hanım ın büyük oğlu Ferit, okuldan arkadaşı olan Alev i sevmektedir. Ancak Alev, zengin bir fabrikatör olan Saim Bey in kızıdır. Her ne kadar Alev babasını pek sevmese de, onun sıkı denetimi altında yaşamaktadır. Saim Bey, kızının Ferit ten ayrılması için aileyi zor durumda bırakır...',
+        image:'images/bizimaile.jpg'
+    },
+
+    {
+        name:'Afrika nın En Tehlikeli Nehri 1.BÖLÜM',
+        des:'Bu Tehlikeli Maceraya Var Mısın?',
+        image:'images/geo.jpg'
+    }
+]
